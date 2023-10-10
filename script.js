4f9bcdbb06835c9f3c4b047f9177ef0506b2d6a7
Oke Hermanns <okehermanns11@gmail.com>
2023-10-10 22:06:54 +0200

    Let player set his marker on a field

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const gameboard = {
 const player = (name, mark) => {
     return {name, mark};
 };
+
+const makeMove = (player, row, col) => {
+    if (gameboard.board[row][col] === '') {
+        gameboard.board[row][col] = player.mark;
+        return true;
+    } else {
+        return false;
+    }
+};
